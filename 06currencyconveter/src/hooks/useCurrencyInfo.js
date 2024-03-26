bf0e6110cdcf308ef2343d7d18a1394bc34ba346
Vineet <dagavineet2001@gmail.com>
2024-03-26 12:26:55 +0530

    react

---
+++ 06currencyconveter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,14 @@
+import {useEffect, useState} from "react"
+
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://v6.exchangerate-api.com/v6/391a0c4040283eff1559e719/latest/${currency}`)
+        .then((res) => res.json())
+        .then((res) => setData(res["conversion_rates"]))
+    }, [currency])
+    return data
+}
+
+export default useCurrencyInfo;
