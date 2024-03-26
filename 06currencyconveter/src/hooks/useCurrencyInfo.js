5e678dd32252e0b0d9c9fd43063c337dd51ef546
Vineet <dagavineet2001@gmail.com>
2024-03-26 12:31:52 +0530

    react

--- 06currencyconveter/src/hooks/useCurrencyInfo.js
+++ 06currencyconveter/src/hooks/useCurrencyInfo.js
@@ -2,7 +2,9 @@ import {useEffect, useState} from "react"
 
 
 function useCurrencyInfo(currency){
+    //this data is used to hold the information about the currency conversion rate in the form of objet 
     const [data, setData] = useState({})
+    // useeffect is used to do execute the function if their is an change in the depenedencies giev 
     useEffect(() => {
         fetch(`https://v6.exchangerate-api.com/v6/391a0c4040283eff1559e719/latest/${currency}`)
         .then((res) => res.json())
@@ -10,5 +12,7 @@ function useCurrencyInfo(currency){
     }, [currency])
     return data
 }
+
+//we ftech the API converth the API into Json form and then select the data thet we needed form that api
 
 export default useCurrencyInfo;
